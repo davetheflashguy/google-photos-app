3dcc96295ca501d3609f8ab78aa45b95c869fb63
Dave Collier <davetheflashguy@gmail.com>
2018-11-10 10:39:33 -0500

    Express FTW

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 let config = require('./config/config.js');
+let express = require('express');
+
+let app = express();
+const PORT  = process.env.PORT || '8080'; // needs to match google api callback config
+
+app.listen(PORT, () => {
+    console.log('Server started on port ', PORT);
+});
 
 console.log('hello world');
